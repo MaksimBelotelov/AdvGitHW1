8f10bcb029c8727885c73b1edee61b6d23792d51
MaksimBelotelov <118625282+MaksimBelotelov@users.noreply.github.com>
2023-11-29 12:30:26 +0300

    Add conflict to AnimalsApplication.java at line 13

--- src/main/java/com/example/cw0301/AnimalsApplication.java
+++ src/main/java/com/example/cw0301/AnimalsApplication.java
@@ -10,7 +10,7 @@ public class AnimalsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AnimalsApplication.class.getResource("animals-view.fxml"));
-
+        String thereWillBeConflict;
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Animals");
         stage.setScene(scene);
